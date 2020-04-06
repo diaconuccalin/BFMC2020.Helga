92334e5523ace0cceb13bf60d12bfdd48b32a9fb
calin <diaconuccalin@gmail.com>
2020-04-07 01:53:02 +0300

    Lane keeping - calibration for elimnation round

--- imageprocessing/lateralcontrol/laneKeeping.py
+++ imageprocessing/lateralcontrol/laneKeeping.py
@@ -13,7 +13,7 @@ from simple_pid         import PID
 from templates.workerprocess import WorkerProcess
 
 class LaneKeeping(WorkerProcess):
-    pid = PID(Ki = 0.75, Kd = 0.0)
+    pid = PID(Ki = 4.75, Kd = 0.0)
     
     # ===================================== Worker process =========================================
     def __init__(self, inPs, outPs):
