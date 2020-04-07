c25d217ad5f4a59e7d6c5f41972102e9308b1736
calin <diaconuccalin@gmail.com>
2020-04-08 02:33:09 +0300

    Sign detection - calibration for elimnation round

--- imageprocessing/signdetection/signDetection.py
+++ imageprocessing/signdetection/signDetection.py
@@ -330,7 +330,7 @@ class SignDetection(WorkerProcess):
 
         # Make second check based on shape
         for blueSign in blueSigns:
-            if isinstance(blueSign, (list, np.ndarray)) and (blueSign is not None) and isParking(blueSign) < 0.1:
+            if isinstance(blueSign, (list, np.ndarray)) and (blueSign is not None) and isParking(blueSign) < 0.5:
                 self.parkCount += 1
                 if self.parkCount > 10:
                     for outP in outPs:
@@ -338,7 +338,7 @@ class SignDetection(WorkerProcess):
                 print("Parking")
             if isinstance(blueSign, (list, np.ndarray)) and (blueSign is not None) and isCrosswalk(blueSign) < 1.3:
                 self.crossCount += 1
-                if self.crossCount > 8 and self.firstCross is True :
+                if self.crossCount > 3 and self.firstCross is True :
                     self.firstCross = False
                     for outP in outPs:
                         outP.send(1)
